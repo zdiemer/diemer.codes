e5f809929f8eefab9541430d3c584a7d5e399f6c
Zach Diemer <zach.diemer@gmail.com>
2018-03-08 22:05:22 -0500

    Made a rudimentary site!

--- src/Site.js
+++ src/Site.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import Header from "./Header";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+
+import {
+  faTwitterSquare,
+  faFacebookSquare,
+  faGithubSquare,
+  faLinkedin
+} from "@fortawesome/fontawesome-free-brands";
+
+import Zach from "./zach.png";
+import "./site.css";
 
 class Site extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-      </div>
+      <div className="site-content">
+        <div className="name">Zachary Diemer</div>
+
+        <div className="description">
+          Software Engineer. Tinkerer. Lifelong student.
+        </div>
+
+        <div className="social">
+          <a className="icon" href="https://twitter.com/zach_diemer">
+            <FontAwesomeIcon icon={faTwitterSquare} />
+          </a>
+
+          <a className="icon" href="https://www.facebook.com/zdiemer">
+            <FontAwesomeIcon icon={faFacebookSquare} />
+          </a>
+
+          <a className="icon" href="https://github.com/zdiemer">
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </a>
+
+          <a className="icon" href="https://www.linkedin.com/in/zach-diemer/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+
+        <img src={Zach} alt="Zach Diemer" />
+      </div >
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Site from './Site';
 import registerServiceWorker from './registerServiceWorker';
 
