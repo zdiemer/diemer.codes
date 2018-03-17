931e2057b2a3f7271b741d871b5308b0e4bb97d6
Zach Diemer <zach.diemer@gmail.com>
2018-03-16 23:06:14 -0400

    [Fix] Minor corrections and CSS changes

--- src/Site.js
+++ src/Site.js
@@ -5,7 +5,13 @@ import {
   faTwitterSquare,
   faFacebookSquare,
   faGithubSquare,
-  faLinkedin
+  faLinkedin,
+  faJs,
+  faNodeJs,
+  faPython,
+  faNode,
+  faReact,
+  faChrome
 } from "@fortawesome/fontawesome-free-brands";
 
 import { faHeart } from "@fortawesome/fontawesome-free-solid";
@@ -34,17 +40,17 @@ class Site extends Component {
           <div className="about-me-body">
             <h1>About Me</h1>
             <div className="about-me-content">
-              <p><span style={{ "font-family": "monospace", "font-weight": "bold" }}>Hello World!</span> My name is Zachary Diemer, and I'm a software engineer. I graduated from <span style={{ color: "#782f40", background: "#ceb888", "padding-left": "0.25em", "padding-right": "0.25em" }}>Florida State University</span> with a Bachelor's of Science in Computer Science in 2016 and have been knee-deep in code ever since. I consider myself a generalist when it comes to software engineering, and strive to understand as many aspects of the craft as I can--despite the impossibility of such an undertaking. I aspire to write code that has a real and positive impact on the world.</p>
+              <p><span style={{ "font-family": "monospace", "font-weight": "bold" }}>Hello World!</span> My name is Zachary Diemer, and I'm a software engineer. I graduated from <span style={{ color: "#782f40", background: "#ceb888", "padding-left": "0.25em", "padding-right": "0.25em" }}>Florida State University</span> with a Bachelor of Science in Computer Science in 2016 and have been knee-deep in code ever since. I consider myself a generalist when it comes to software engineering, and strive to understand as many aspects of the craft as I can--despite the impossibility of such an undertaking. I aspire to write code that has a real and positive impact on the world.</p>
 
               <img className="zach-nyc" src={ZachNYC} alt="Zach Diemer in NYC" />
 
-              <p>I've always been fascinated by <span style={{ "text-decoration": "underline !important" }}>how things work</span>, and that fascination has permeated into every facet of my life. I currently spend my work days writing code in JavaScript, Node.js, and Python. In my free time, I enjoy tinkering with everything from my collection of Raspberry Pis to optimizing a Minecraft server. Technology defines a big part of who I am, and I'm grateful for the opportunity to embrace it daily.</p>
+              <p>I've always been fascinated by <span style={{ "text-decoration": "underline !important" }}>how things work</span>, and that fascination has permeated into every facet of my life. I currently spend my work days writing code in JavaScript <FontAwesomeIcon style={{ opacity: 0.5 }} icon={faJs} />, Node.js <FontAwesomeIcon style={{ opacity: 0.5 }} icon={faNodeJs} />, and Python <FontAwesomeIcon style={{ opacity: 0.5 }} icon={faPython} />. In my free time, I enjoy tinkering with everything from my collection of Raspberry Pis to optimizing a Minecraft server. Technology defines a big part of who I am, and I'm grateful for the opportunity to embrace it daily.</p>
 
-              <p>Currently, I work as a software engineer at Nielsen in Tampa, FL. Here at Nielsen I've been granted the opportunity to work on a number of exciting projects and I've been exposed to plenty of new technology stacks. Not to mention the wonderful people I've had the privilege and pleasure of working with! I've been able to work on projects as varied as React web applications, Chrome extensions, cloud architecting, and more. It's been a valuable experience so far, and I'm hopeful that the future will bring many more interesting and unique projects.</p>
+              <p>Currently, I work as a software engineer at <span style={{ color: "white", background: "#00aeef", "padding-left": "0.25em", "padding-right": "0.25em" }}>Nielsen</span> in Tampa, FL. Here at Nielsen I've been granted the opportunity to work on a number of exciting projects and I've been exposed to plenty of new technology stacks. Not to mention the wonderful people I've had the privilege and pleasure of working with! I've been able to work on projects as varied as React <FontAwesomeIcon style={{ opacity: 0.5 }} icon={faReact} /> web applications, Chrome <FontAwesomeIcon style={{ opacity: 0.5 }} icon={faChrome} /> extensions, cloud architecting, and more. It's been a valuable experience so far, and I'm hopeful that the future will bring many more interesting and unique projects.</p>
 
-              <p>Aside from Nielsen, I've also worked previously at Rating Dynamics in Tallahassee, FL, where I worked extensively with C#, .NET Framework, and ASP.NET. As my first foray into the field of professional software development, I was exposed to what it's like to actually work in a professional environment, and the lessons I learned from there are lessons that will stick with me for a long time throughout my career. Working on a small team engenders (and requires) a great degree of trust and respect, and Rating Dynamics was certainly a place where I felt a mutual sense of each of these.</p>
+              <p>Aside from Nielsen, I've also worked previously at <span style={{ color: "#037ea9", background: "#bad13b", "padding-left": "0.25em", "padding-right": "0.25em" }}>Rating Dynamics</span> in Tallahassee, FL, where I worked extensively with C#, .NET Framework, and ASP.NET. As my first foray into the field of professional software development, I was exposed to what it's like to actually work in a professional environment, and the lessons I learned from there are lessons that will stick with me for a long time throughout my career. Working on a small team engenders (and requires) a great degree of trust and respect, and Rating Dynamics was certainly a place where I felt a mutual sense of each of these.</p>
 
-              <p>Software development isn't the only aspect of my character. I'm also an avid reader, board gamer, and disc golfer. My Kindle and I are often inseparable, and hardly a weekend goes by without either board gaming or throwing a round of disc golf. I find it valuable to be a balanced individual in every aspect of life, so being pursuing hobbies and growth outside of my career is something which I take extremely seriously.</p>
+              <p>Software development isn't the only aspect of my character. I'm also an avid reader, board gamer, and disc golfer. My Kindle and I are often inseparable, and hardly a weekend goes by without either board gaming or throwing a round of disc golf. I find it valuable to be a balanced individual in every aspect of life, so being able to pursue hobbies and growth outside of my career is something which I take extremely seriously.</p>
 
               <p>I'd love to hear from you if you want to know anything else about me, or if you'd like to chat!</p>
             </div>
